f4a7d9c245e4f365a6ebc9766343c200d4101212
Alexander Graul <agraul@suse.com>
2024-07-22 11:53:04 +0200

    Automatically add theme to custom-theme-load-path

--- acme-theme.el
+++ acme-theme.el
@@ -561,6 +561,15 @@ in echo area."
        (sit-for 1)
        (message "%s" text)))))
 
+
+;;;###autoload
+(when (and (boundp 'custom-theme-load-path) load-file-name)
+  (let* ((base (file-name-directory load-file-name))
+         (dir (expand-file-name "themes/" base)))
+    (add-to-list 'custom-theme-load-path
+                 (or (and (file-directory-p dir) dir)
+                     base))))
+
 (run-hooks 'acme-theme-hook)
 (provide-theme 'acme)
 ;;; acme-theme.el ends here
